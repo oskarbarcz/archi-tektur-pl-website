1892335e734a692014a140880e438fb0e88f2373
archi-tektur <kontakt@archi-tektur.pl>
2019-05-12 00:16:53 +0200

    Installed Encore and PHP dev web server

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,7 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class  => ['all' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class  => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
