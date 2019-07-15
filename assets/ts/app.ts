28d4d329b5cfbe12f8bd438746d7f9cff3f0c635
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-07-15 17:52:46 +0200

    Added TypeScript menu actions

--- assets/ts/app.ts
+++ assets/ts/app.ts
@@ -1,15 +1,4 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you require will output into a single css file (app.css in this case)
 require('../sass/main.sass');
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.ts');
 
@@ -24,16 +13,31 @@ function autosize() {
 
 // define menu objects
 let hamburger: HTMLElement = document.querySelector('.navigation_hamburger');
-let navigationLinks: HTMLElement = document.querySelector('.navigation_links');
-let cssClass = 'navigation_links--opened';
+let links: HTMLElement = document.querySelector('.navigation_links');
+let openedClass = 'navigation_links--opened';
 
-window.addEventListener('click', (event) => {
-    // if opened and target is NOT menu
-    if (event.target == hamburger) {
-        navigationLinks.classList.toggle(cssClass);
-    }
-
-    if (navigationLinks.classList.contains(cssClass) && event.target !== navigationLinks) {
-        navigationLinks.classList.remove(cssClass);
+hamburger.addEventListener('click', (event) => {
+    if (!links.classList.contains(openedClass)) {
+        // if closed
+        links.classList.add(openedClass);
+        iconSwitch(event, false);
+        window.addEventListener('click', closeOnClickOut)
+    } else {
+        // if opened
+        window.removeEventListener('click', closeOnClickOut);
+        iconSwitch(event, true);
+        links.classList.remove(openedClass);
     }
 });
+
+let closeOnClickOut = (event) => {
+    if (event.target != links && event.target != hamburger) {
+        links.classList.remove(openedClass);
+    }
+};
+
+let iconSwitch = (event, toClose) => {
+    let iconClose = 'fa-times';
+    let iconOpen = 'fa-bars';
+    toClose ? event.target.classList.replace(iconClose, iconOpen) : event.target.classList.replace(iconOpen, iconClose);
+};
