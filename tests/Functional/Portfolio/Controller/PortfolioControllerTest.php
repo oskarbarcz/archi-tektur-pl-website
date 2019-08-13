25c7d24468f58bbd828e99c2a33637876db55d27
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-08-13 22:31:48 +0200

    Reassigned tests to make it comply with new architecture

--- tests/Functional/Portfolio/Controller/PortfolioControllerTest.php
+++ tests/Functional/Portfolio/Controller/PortfolioControllerTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace App\Tests\Functional\Controller;
+namespace App\Tests\Functional\Portfolio\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
