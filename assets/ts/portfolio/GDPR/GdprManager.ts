a7fbab6514c80a457e00b9f587217346279a401b
Oskar 'archi_tektur' Barcz <kontakt@archi-tektur.pl>
2019-09-10 18:33:51 +0200

    Added cookies info working TS mechanism

--- assets/ts/portfolio/GDPR/GdprManager.ts
+++ assets/ts/portfolio/GDPR/GdprManager.ts
@@ -1,30 +1,47 @@
+let Cookies = require('js-cookie/src/js.cookie.js');
+
 class GdprManager {
 
     _box: HTMLElement;
     _closeButton: HTMLElement;
 
+    readonly COOKIE_NAME = 'ACCEPT_GDPR';
+
+    /**
+     *
+     * @param box whole box that stores info
+     * @param closeButton button to set event listener
+     */
     public constructor(box: HTMLElement, closeButton: HTMLElement) {
         this._box = box;
         this._closeButton = closeButton;
-        this.handle();
-    }
+        const cookieValue = Cookies.get(this.COOKIE_NAME);
 
-    private static removeElement(element: HTMLElement) {
-        element.remove();
+        console.log(cookieValue);
+
+        if (cookieValue) {
+            // user clicked before
+            this._box.remove();
+
+        } else {
+            // user did not clicked before
+            this._box.classList.add('cookies-visible');
+            this.handle();
+        }
     }
 
     private handle() {
         this._closeButton.addEventListener('click', () => {
             // toggle class
-            this._box.classList.add('cookies--invisible');
-
+            this._box.classList.add('cookies--visible');
+            // add cookie
+            Cookies.set(this.COOKIE_NAME, true);
             // remove after 100ms
             setTimeout(() => {
-                GdprManager.removeElement(this._box);
+                this._box.remove();
             }, 100);
         });
     }
-
 }
 
 export {GdprManager};
