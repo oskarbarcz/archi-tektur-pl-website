6574eff134c81e014d9105d2892d1568bd5c9539
Oskar Barcz <kontakt@archi-tektur.pl>
2021-04-27 09:05:13 +0200

    Clear project for clearer intentions

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,33 @@
-var Encore = require('@symfony/webpack-encore');
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
+}
 
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
-    .addEntry('app', './assets/js/app.js')
-  .enableSingleRuntimeChunk()
-  .cleanupOutputBeforeBuild()
-  .enableBuildNotifications()
-  .enableSourceMaps(!Encore.isProduction())
-  .enableVersioning(Encore.isProduction())
-  .configureBabel(() => {}, {
-    useBuiltIns: 'usage',
-    corejs: 3,
-  })
-  .enableSassLoader()
-  .enableIntegrityHashes();
+    /* ENTRY CONFIG */
+    .addEntry('landing', './assets/scripts/landing.js')
+    /* FEATURE CONFIG */
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+    .enableSassLoader()
+    .copyFiles({
+        from: './assets/images',
+        to: 'images/[path][name].[hash:8].[ext]'
+    })
+    .enableIntegrityHashes(Encore.isProduction());
 
 module.exports = Encore.getWebpackConfig();
